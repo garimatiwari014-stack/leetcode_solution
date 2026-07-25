6e67881b137f6d85d616da1c6121c9e6b937724d
garimatiwari014-stack <garimatiwari014@gmail.com>
2026-07-25 11:29:49 +0530

    Leetcode_63

---
+++ leetcode_63.java
@@ -0,0 +1,23 @@
+public class leetcode_63{
+public class Solution {
+    public int maxProduct(int n) {
+        int d1 = 0;
+        int d2 = 0;
+
+        while (n > 0) {
+            int rem = n % 10;
+
+            if (rem >= d1) {
+                d2 = d1;
+                d1 = rem;
+            } else if (rem > d2) {
+                d2 = rem;
+            }
+
+            n /= 10;
+        }
+
+        return d1 * d2;
+    }
+}
+}
