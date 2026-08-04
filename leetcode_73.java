0ddd9de6829f4d89c81f588888e95f56ac727de6
garimatiwari014-stack <garimatiwari014@gmail.com>
2026-08-04 11:26:25 +0530

    Add files via upload

---
+++ leetcode_73.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        // Store all numbers and find min & max
+        for (int num : nums) {
+            set.add(num);
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        // Check every number in the range
+        for (int i = min; i <= max; i++) {
+            if (!set.contains(i)) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+}
