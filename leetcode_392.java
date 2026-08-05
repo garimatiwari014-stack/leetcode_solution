4ed6d550e7aaccd430799d89008b7a48494506a9
garimatiwari014-stack <garimatiwari014@gmail.com>
2026-08-05 19:33:28 +0530

    Add files via upload

--- leetcode_392.java
+++ leetcode_392.java
@@ -1,20 +1,13 @@
-class leetcode_392{
-
+class Solution {
     public boolean isSubsequence(String s, String t) {
         int i = 0;
         int j = 0;
-        while (i < s.length() && j < t.length()) {
-            if (s.charAt(i) == t.charAt(j)) {
-                i++;
-            }
-            j++;
+        while(i < s.length() && j < t.length()){
+          if( s.charAt(i)== t.charAt(j)){
+               i++;
+          }
+          j++;
         }
-        return i == s.length();
+return i == s.length();
     }
-    public static void main(String [] args){
-        String s = "abc";
-        String t = "ahbgdc";
-    }
-        
-    
 }
