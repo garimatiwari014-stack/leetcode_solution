0e3c0f5454af74b23c627073159415575d2c97aa
garimatiwari014-stack <garimatiwari014@gmail.com>
2026-08-10 09:18:20 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+
+    public boolean winnerSquareGame(int n) {
+
+        boolean[] dp = new boolean[n + 1];
+
+        dp[0] = false;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j * j <= i; j++) {
+
+                int square = j * j;
+
+                if (dp[i - square] == false) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}  
