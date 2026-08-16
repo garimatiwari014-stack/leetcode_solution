5f6f7b35f98ff7d8b28e31dc35a71504f071f9fc
garimatiwari014-stack <garimatiwari014@gmail.com>
2026-08-16 10:53:45 +0530

    Add files via upload

---
+++ leetcode_85.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+
+        int cnt0 = 0;
+        int cnt1 = 0;
+        int cnt2 = 0;
+
+        for (int stone : stones) {
+            if (stone % 3 == 0) {
+                cnt0++;
+            } else if (stone % 3 == 1) {
+                cnt1++;
+            } else {
+                cnt2++;
+            }
+        }
+
+        if (cnt0 % 2 == 0) {
+            return cnt1 > 0 && cnt2 > 0;
+        }
+
+        return Math.abs(cnt1 - cnt2) > 2;
+    }
+}
