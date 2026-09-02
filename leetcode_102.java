ca90a68ae7a9dafa9b21b1e6c487e1a558a6413b
garimatiwari014-stack <garimatiwari014@gmail.com>
2026-09-02 14:39:11 +0530

    Add files via upload

---
+++ leetcode_102.java
@@ -0,0 +1,12 @@
+class leetcode_102 {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+
+
+    public static void main(String [] args){
+        int [] nums = {2,3,4};
+        leetcode_102 s = new leetcode_102();
+        System.out.println(s.uniformArray(nums));
+    }
+}
