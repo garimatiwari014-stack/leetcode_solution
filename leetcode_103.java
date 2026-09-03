1d09a019bbff6f109b25c06b5b985640d9ac3367
garimatiwari014-stack <garimatiwari014@gmail.com>
2026-09-03 23:26:19 +0530

    Add files via upload

---
+++ leetcode_103.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPossible(int[] nums1) {
+
+        int min = Integer.MAX_VALUE;
+
+        for (int x : nums1) {
+            min = Math.min(min, x);
+        }
+
+        if (min % 2 == 1) {
+            return true;
+        }
+
+        for (int x : nums1) {
+            if (x % 2 == 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
